b340f3256c0a933b652c9500036f1bfa03efa86d
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-04 09:41:18 +0200

    creazione array link navbar

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,14 @@
 import { reactive } from 'vue';
 
 const ilMioOggettone = {
-
+    linkHeader: [
+        'Home',
+        'About',
+        'Service',
+        'Showcase',
+        'Blog',
+        'Contact'
+    ]
 };
 
 const iMieiDati = reactive(ilMioOggettone);
