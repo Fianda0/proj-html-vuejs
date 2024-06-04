d2339281de3a4d4f7144f4fd6542951f4d31b104
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-04 09:40:28 +0200

    Aggiunta file

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+import store from '../data/store.js'
+
+export default {
+    name: AppHeader,
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
